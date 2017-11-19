73d5544d342bc3b9b1ca22ed0df1286633b3859c
Arévalo Araque <aaraque@analyze.cl>
2017-11-18 23:44:15 -0300

    Agregando configuracion log

--- microservicio_valor_uf/settings.py
+++ microservicio_valor_uf/settings.py
@@ -112,12 +112,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL       = '/static/'
 STATIC_ROOT      = os.path.abspath( os.path.join( BASE_DIR, '..', 'static-microservicios-uf' ) )
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+MEDIA_ROOT       = os.path.join(BASE_DIR, "media")
 
 
 # local settings
